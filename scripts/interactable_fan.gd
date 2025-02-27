4058f74d4208956a5ee45aab6d26fe9f19584929
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2025-02-27 09:07:54 +0300

    various fixes, updated UI

--- scripts/interactable_fan.gd
+++ scripts/interactable_fan.gd
@@ -3,14 +3,14 @@ extends Area3D
 
 var is_threat:=false
 var is_used :=false
-var fan_anim
-var fan_sound
+@onready var fan_anim = get_parent().get_node("FanAnimation")
+@onready var fan_sound = get_parent().get_node("FanNoise")
+@onready var fan_light = get_parent().get_node("FanLight")
+@onready var fan_self_light = get_parent().get_node("FanSelfLight")
 var playing := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fan_anim = get_parent().get_node("FanAnimation")
-	fan_sound = get_parent().get_node("FanNoise")
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
@@ -23,8 +23,12 @@ func in_threat():
 	if fan_sound.playing == false:
 		fan_sound.play()
 	fan_anim.play("play")
+	fan_light.show()
+	fan_self_light.show()
 	
 func not_in_threat():
 	if fan_sound.playing:
 		fan_sound.stop()
 	fan_anim.stop()	
+	fan_light.hide()
+	fan_self_light.hide()
